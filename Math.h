df4eb6b67972e5b64f9bc7fa51abf329e2cb7fd7
Dan Schmidt <dfan@dfan.org>
2013-07-17 21:04:56 -0400

    TransformState keeps track of matrices

--- Math.h
+++ Math.h
@@ -300,18 +300,26 @@ class TransformState
 public:
    TransformState();
 
-   void SetScale( const Vector3f& scale ) { mScale = scale; }
-   void SetTranslation( const Vector3f& translation ) { mTranslation = translation; }
-   void SetRotation( const Vector3f& rotation ) { mRotation = rotation; }
+   void SetScale( const Vector3f& scale );
+   void SetTranslation( const Vector3f& translation );
+   void SetRotation( const Vector3f& rotation );
 
    const Vector3f& GetScale() { return mScale; }
    const Vector3f& GetTranslation() { return mTranslation; }
    const Vector3f& GetRotation() { return mRotation; }
 
 public:
+   void ComputeXfm();
+
    Vector3f mScale;
    Vector3f mTranslation;
    Vector3f mRotation;
+
+   Matrix4f mScaleXfm;
+   Matrix4f mTranslationXfm;
+   Matrix4f mRotationXfm;
+
+   Matrix4f mXfm;               // composition
 };
 
 //////////////////////////////
